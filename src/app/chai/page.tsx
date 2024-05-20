5c8343e884960911fb3ed160eac1589db3cc80bb
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-20 07:04:02 -0700

    applied Button from shedcn

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+function chaipage() {
+    return (
+        <main className="h-screen flex items-center justify-center flex-col ">
+            <div className="my-5">
+                chaipage 😁
+            </div>
+            <Button variant={"jerry"} size={"lg"}>Shedcn</Button>
+        </main>
+    )
+}
+
+export default chaipage
